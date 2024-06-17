b92b99e7f35b2fd4bf061c5ce49b6b106c64f29e
Sadig Imanov <sadiqimanov69@gmail.com>
2024-06-17 16:45:04 +0400

    cars slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,49 +5,51 @@ const carImg = document.getElementById('carImg');
 const cars = [{
     title: '2016 Mustang GT, San Francisco, United States',
     explain: 'Published on May 17, 2017',
-    photo: 'assets/img/mustang.webp'
+    photo: 'mustang.webp'
 },
 {
     title: 'Nissan Skyline Gtr R34, Japan',
     explain: 'Published on April 26, 1957',
-    photo: 'assets/img/skyline.jpg'
+    photo: 'skyline.jpg'
 },
 {
     title: 'Mazda RX-7 Spirit R, Hiroshima, Japan',
     explain: 'Published on March 3, 2002 ',
-    photo: 'assets/img/mazda.jpg'
+    photo: 'mazda.jpg'
 },
 {
     title: 'Bugatti Chiron, Molsheim, France',
     explain: 'Published on May 26, 2016 ',
-    photo: 'assets/img/chiron.webp'
+    photo: 'chiron.webp'
 },
 {
     title: 'Toyota Supra from "The Fast and the Furious"',
     explain: 'Published on Octaber 26, 1994 ',
-    photo: 'assets/img/supra.jpg'
+    photo: 'supra.jpg'
 },
 {
     title: 'Lightning McQueen, United States',
     explain: 'Published on November 26, 2006 ',
-    photo: 'assets/img/mcqueen.jpg'
+    photo: 'mcqueen.jpg'
 },
 ];
 let count = 0;
 
 function render(index) {
-    carImg.src = cars[index].photo;
-    title.textContent = cars[index].title;
-    explain.textContent = cars[index].explain;
-
-}
+    const carIndex = cars[index]
+    carImg.src = `./assets/img/${carIndex.photo}`;
+    title.textContent = carIndex.title;
+    explain.textContent = carIndex.explain;
+};
 
 function nextBtn() {
-    count = (count + 1) % cars.length;
+    count++;
+    if (count === cars.length) count = 0;
     render(count);
-}
+};
 
 function prevBtn() {
-    count = (count - 1 + cars.length) % cars.length;
+    count--;
+    if (count < 0) count = cars.length - 1;
     render(count);
-}
+};
